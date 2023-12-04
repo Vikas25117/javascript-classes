65a9d67def935240dd202809f07b35e6f58def8c
vicky <khalsavicky1999@gmail.com>
2023-12-04 10:19:30 +0530

    i have done my modules section

--- js/modules-two.js
+++ js/modules-two.js
@@ -11,6 +11,15 @@ export function userFunction(name) {
 // javascript modules class section 
 export class modulesClass {
     constructor() {
-        console.log("constructor methods");
+        console.log("constructor methods ");
     }
 };
+
+// ============= Notes this page ===================
+/**
+ * hm apni files me export ko one place per export kr skte hai hme har bar export likhne ki need nhi hai 
+ * example below like this esme hmne sbi export ki one hi palce per export kra diya hai hm chae to auper likhe export ko cut ke skte hai or niche likhe export ko uncomment kr dage 
+ * 
+ * ++++++++++++++++ export { message, user, userFunction, modulesClass }; +++++++++++++++++++++++
+ *  =============== hm export ko default v use kr skte hai per eske lye app chatGPT ja google per search kre 
+*/
--- js/modules.js
+++ js/modules.js
@@ -1,3 +1,26 @@
+// ==================== Notes This page ===========================
+/**
+ * hm import me jo function import kra rkhe hai onke name ko sort kr skte hai like this 
+ * jase ki niche function import hai userFunction es function ka name kaffi big hai or ma ese sort kr skta hu aalice method ki help se 
+ * import { message, user, userFunction as usFu, modulesClass } from "./modules-two.js";
+ * ab userFunction :- usFu k name se change ho chuka hai ab hm ese apne new name se call krege jase ki 
+ * usFu();
+ * 
+ * ==================== second note ===================
+ * hamare pass modules-two.js me kaffi kuj hai jase ki variabels, classes, function exc.. hm un sbi ko hi apni es file me use krna chate hai to hm inko globaly import kr skte hai * ka use krke or esme hm import se pehale * k sath aalice methods ki help se aak name create krege jo hme import krne me help krega example below like this 
+ * 
+ * import * as vikas from "./modules-two.js";
+ * ab hmne yha dusri file k sbi classes or varibels or functions kause kr liya hai ab hm enko niche likhe anusar use kr skte hai jase kki 
+ *  console.log(vikas.message);
+ * vikas.user();
+ * vikas.userFunction("user function vikas");
+ * let modulesClassResult = new vikas.modulesClass();  
+ * es trike se hm enko use kr skte hai 
+ */
+
+
+
+
 import { message, user, userFunction, modulesClass } from "./modules-two.js";
 console.log(message);
 /**document.body.innerHTML = message;
